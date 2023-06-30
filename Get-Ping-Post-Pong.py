12d4dc7da437df28bdab9e41bcd650dfafc0412c
DanielShvch <122821595+DanielShvch@users.noreply.github.com>
2023-06-30 09:11:56 +0300

    Update Get-Ping-Post-Pong.py

--- Get-Ping-Post-Pong.py
+++ Get-Ping-Post-Pong.py
@@ -1,1 +1,28 @@
+#main.py
+from flask import Flask, request
 
+# main.py
+app = Flask(__name__)
+
+
+@app.route('/ping', methods=['GET', 'POST'])
+def ping():
+    if request.method == "GET":
+        return {
+            'message': 'This endpoint should return a list of entities' ,
+            'method': request.method ,
+            'body': request.json ,
+            'path': request.path ,
+            'URL':  request.host_url 
+
+        }
+    if request.method == "POST":
+        return {
+            'message': 'This endpoint should create an entity',
+            'method': request.method,
+		    'body': request.json
+        }
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
