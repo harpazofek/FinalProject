3d30538e3e6098490bd5da565c099c97f4956713
Ofek Harpaz <111492589+harpazofek@users.noreply.github.com>
2023-08-29 18:46:13 +0300

    .

--- ping-pong-app/ping-pong.py
+++ ping-pong-app/ping-pong.py
@@ -8,10 +8,6 @@ from logconfig import setup_logging
 # Set up logging
 setup_logging()
 
-def main():
-    app.run(host="0.0.0.0", port=5200, debug=True)
-
-
 app = Flask(__name__)
 
 # Get MongoDB credentials from environment variables
@@ -61,6 +57,44 @@ def pingpongfun():
     if request.method == 'POST':
         return '<h1> Ofek say\'s P0NG</h1>'
 
+# In-memory storage for countries
+countries = [
+        {"name": "United States", "capital": "Washington, D.C.", "population": 331002651},
+        {"name": "Canada", "capital": "Ottawa", "population": 37742154},
+        {"name": "United Kingdom", "capital": "London", "population": 67886011},
+        {"name": "Australia", "capital": "Canberra", "population": 25499884},
+        {"name": "India", "capital": "New Delhi", "population": 1380004385}
+    ]
+
+@app.route('/countries', methods=['GET'])
+def get_countries():
+    return jsonify(countries)
+
+@app.route('/countries', methods=['POST'])
+def add_country():
+    data = request.get_json()
+    countries.append(data)
+    return jsonify({"message": "Country added successfully"})
+
+@app.route('/countries/<int:index>', methods=['PUT'])
+def update_country(index):
+    if index >= 0 and index < len(countries):
+        data = request.get_json()
+        countries[index] = data
+        return jsonify({"message": "Country updated successfully"})
+    else:
+        return jsonify({"message": "Invalid index"})
+
+@app.route('/countries/<int:index>', methods=['DELETE'])
+def delete_country(index):
+    if index >= 0 and index < len(countries):
+        del countries[index]
+        return jsonify({"message": "Country deleted successfully"})
+    else:
+        return jsonify({"message": "Invalid index"})
+
+def main():
+    app.run(host="0.0.0.0", port=5200, debug=True)
 
 if __name__ == '__main__':
     main()
