247e328f837d629209a7395e696de9f497f0684f
Ofek Harpaz <111492589+harpazofek@users.noreply.github.com>
2023-09-06 22:52:07 +0300

    forking to my final project

--- app/ping-pong.py
+++ app/ping-pong.py
@@ -47,9 +47,15 @@ def save_request():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+########### Application Routing #############
+
 @app.route('/ping', methods=['GET'])
 @app.route('/pong', methods=['POST'])
 @app.route('/save_request', methods=['POST', 'GET'])
+@app.route('/basic_info', methods=['GET'])
+@app.route('/basic_info_add', methods=['POST'])
+@app.route('/basic_info_put', methods=['PUT'])
+@app.route('/basic_info_del', methods=['DELETE'])
 
 def pingpongfun():
     if request.method == 'GET':
@@ -57,41 +63,56 @@ def pingpongfun():
     if request.method == 'POST':
         return '<h1> Ofek say\'s P0NG</h1>'
 
-# In-memory storage for countries
-countries = [
-        {"name": "United States", "capital": "Washington, D.C.", "population": 331002651},
-        {"name": "Canada", "capital": "Ottawa", "population": 37742154},
-        {"name": "United Kingdom", "capital": "London", "population": 67886011},
-        {"name": "Australia", "capital": "Canberra", "population": 25499884},
-        {"name": "India", "capital": "New Delhi", "population": 1380004385}
+@app.route('/ping', methods=['GET'])
+def ping():
+    if request.method == "GET":
+        return f'<H2> Method GET, you have pinged successfuly   <br><br> Have a nice DAY and pong to you <H2> \
+                URL: {request.host_url}ping \
+                <br><br> Project BY : Daniel, Eli, Ofek, Lior'  
+
+
+@app.route('/pong', methods=['POST'])
+def pong():
+    if request.method == "POST":
+        return f'<H2> Method POST, you have pinged successfuly  <br><br> Have a nice DAY <H2> \
+               URL: {request.host_url}pong \
+                <br><br> Project BY : Daniel, Eli, Ofek, Lior'  
+
+# Storage for basic information
+basic_info = [
+        {"name": "Eli Levi", "city": "Rosh Hain", "ID": 331546518},
+        {"name": "Daniel Shahnovich", "city": "Karnei Shomron", "ID": 324578981},
+        {"name": "Lior Taub", "city": "Ramat Gan", "ID": 615478536},
+        {"name": "Ofek Harpaz", "city": "Tel Aviv", "ID": 308696938},
     ]
 
-@app.route('/countries', methods=['GET'])
-def get_countries():
-    return jsonify(countries)
+@app.route('/basic_info', methods=['GET'])
+def get_basic_info():
+    return jsonify(basic_info)
 
-@app.route('/countries', methods=['POST'])
-def add_country():
+@app.route('/basic_info_add', methods=['POST'])
+def add_basic_info():
     data = request.get_json()
-    countries.append(data)
-    return jsonify({"message": "Country added successfully"})
+    basic_info.append(data)
+    return jsonify({"message": "Basic info added successfully"})
 
-@app.route('/countries/<int:index>', methods=['PUT'])
-def update_country(index):
-    if index >= 0 and index < len(countries):
+@app.route('/basic_info_put/<int:index>', methods=['PUT'])
+def update_basic_info(index):
+    if index >= 0 and index < len(basic_info):
         data = request.get_json()
-        countries[index] = data
-        return jsonify({"message": "Country updated successfully"})
+        basic_info[index] = data
+        return jsonify({"message": "Basic info updated successfully"})
     else:
         return jsonify({"message": "Invalid index"})
 
-@app.route('/countries/<int:index>', methods=['DELETE'])
-def delete_country(index):
-    if index >= 0 and index < len(countries):
-        del countries[index]
-        return jsonify({"message": "Country deleted successfully"})
+@app.route('/basic_info_del/<int:index>', methods=['DELETE'])
+def delete_basic_info(index):
+    if index >= 0 and index < len(basic_info):
+        del basic_info[index]
+        return jsonify({"message": "Basic info deleted successfully"})
     else:
         return jsonify({"message": "Invalid index"})
+
 
 def main():
     app.run(host="0.0.0.0", port=5200, debug=True)
